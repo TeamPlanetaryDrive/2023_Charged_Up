e0ce8baa2c7e17078096c2708d767cfc2776a3aa
ddozzi <107476445+krozzi@users.noreply.github.com>
2023-01-13 15:54:18 -0500

    idk

---
+++ src/main/java/frc/robot/subsystems/AprilTagVision.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagDetector;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AprilTagVision extends SubsystemBase {
+    public AprilTagVision() {
+    }
+
+
+}
