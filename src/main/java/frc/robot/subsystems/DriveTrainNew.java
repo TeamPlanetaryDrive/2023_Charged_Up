e1c934daec0308467271698e4faf6f42ee395a3a
Mae671 <mae.mobley@stu.fayette.kyschools.us>
2023-01-13 19:41:52 -0500

    adding drivetrain file

---
+++ src/main/java/frc/robot/subsystems/DriveTrainNew.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DriveTrainNew extends SubsystemBase {
+    
+}
