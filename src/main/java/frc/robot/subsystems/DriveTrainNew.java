f584ad14054152b3b65444ccd3999488c9ef5ecc
Mae671 <mae.mobley@stu.fayette.kyschools.us>
2023-01-13 19:51:09 -0500

    Adds constructor

--- src/main/java/frc/robot/subsystems/DriveTrainNew.java
+++ src/main/java/frc/robot/subsystems/DriveTrainNew.java
@@ -4,10 +4,25 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 import frc.robot.RobotMap;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class DriveTrainNew extends SubsystemBase {
+    DifferentialDrive robotDrive;
+    Victor lMotor, rMotor;
+    public Encoder encoderL, encoderR;
     
+    public DriveTrainNew() {
+        super();
+        lMotor = new Victor(RobotMap.LEFT_MOTOR_CHANNEL);
+        rMotor = new Victor(RobotMap.RIGHT_MOTOR_CHANNEL);
+        rMotor.setInverted(true);
+        robotDrive = new DifferentialDrive(lMotor, rMotor);
+        robotDrive.setSafetyEnabled(false);
+        encoderL = new Encoder(RobotMap.DRIVETRAIN_ENCODER_CHANNEL_L_A, RobotMap.DRIVETRAIN_ENCODER_CHANNEL_L_B);
+        encoderR = new Encoder(RobotMap.DRIVETRAIN_ENCODER_CHANNEL_R_A, RobotMap.DRIVETRAIN_ENCODER_CHANNEL_R_B, true);
+      }
 }
